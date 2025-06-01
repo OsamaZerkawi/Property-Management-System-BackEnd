948dd4d3460a00e32129358d4812f9b568b71116
Hasan <hzaeter04@gmail.com>
2025-06-01 20:00:50 +0300

    fix bug in auth controller

--- src/presentation/http/controllers/auth.controller.ts
+++ src/presentation/http/controllers/auth.controller.ts
@@ -1,10 +1,8 @@
 import { Body, Controller, Get, HttpCode, HttpStatus, Post, Req, Request, UseGuards } from "@nestjs/common";
-import { REDIRECT_METADATA } from "@nestjs/common/constants";
 import { Request as requsetExpress } from "express";
 import { LoginDto } from "src/application/dtos/auth/login.dto";
 import { LoginUseCase } from "src/application/use-cases/auth/login.use-case";
 import { LogoutUseCase } from "src/application/use-cases/auth/logout.use-case";
-import { User } from "src/domain/entities/user.entity";
 import { CurrentUser } from "src/shared/decorators/current-user.decorator";
 import { JwtAuthGuard } from "src/shared/guards/jwt-auth.guard";
 import { successResponse } from "src/shared/helpers/response.helper";
