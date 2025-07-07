45d2a6c243bcbaee3e8fc7a44a932efc9306555c
Hasan <hzaeter04@gmail.com>
2025-07-07 18:10:06 +0300

    fix bugs

--- src/infrastructure/auth/strategies/mobile_local.strategy.ts
+++ src/infrastructure/auth/strategies/mobile_local.strategy.ts
@@ -10,7 +10,7 @@ import {
   } from 'src/domain/repositories/mobile_auth.repository';
   
 @Injectable()
-export class MobileLocalStrategy extends PassportStrategy(Strategy) {
+export class MobileLocalStrategy extends PassportStrategy(Strategy,'mobile-local') {
   constructor(private readonly validateUser: MobileValidateUserUseCase,
 
   @Inject(MOBILE_AUTH_REPOSITORY)
