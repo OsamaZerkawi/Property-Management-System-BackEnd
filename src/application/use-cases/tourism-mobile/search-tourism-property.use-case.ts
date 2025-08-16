40a95061421d181b8a1ab5e774b3df4eea257488
Hasan <hzaeter04@gmail.com>
2025-08-16 18:41:04 +0300

    fix response of touristic mobile search api

--- src/application/use-cases/tourism-mobile/search-tourism-property.use-case.ts
+++ src/application/use-cases/tourism-mobile/search-tourism-property.use-case.ts
@@ -1,34 +1,36 @@
 // tourism.usecase.ts
-import { Inject, Injectable } from '@nestjs/common'; 
-import { ITourismRepository, TOURISM_REPOSITORY } from 'src/domain/repositories/tourism.repository';
+import { Inject, Injectable } from '@nestjs/common';
+import {
+  ITourismRepository,
+  TOURISM_REPOSITORY,
+} from 'src/domain/repositories/tourism.repository';
 import { successPaginatedResponse } from 'src/shared/helpers/response.helper';
 
 @Injectable()
 export class SearchTourismUseCase {
-  constructor( 
-      @Inject(TOURISM_REPOSITORY)
-      private readonly repo: ITourismRepository, 
+  constructor(
+    @Inject(TOURISM_REPOSITORY)
+    private readonly repo: ITourismRepository,
   ) {}
 
   async execute(
     search: string,
     page = 1,
     items = 10,
-    baseUrl:string,
+    baseUrl: string,
   ): Promise<any> {
     const { data, total } = await this.repo.searchByTitle(search, page, items);
 
     const results = data.map((p) => ({
       propertyId: p.id,
       location: `${p.region.city.name}, ${p.region.name}`,
-       postImage: p.post.image
-      ? `${baseUrl}/uploads/properties/posts/images/${p.post.image}`
-      : null, 
-      title: p.post.title,
-      price: p.touristic.price,
+      postImage: p.post.image
+        ? `${baseUrl}/uploads/properties/posts/images/${p.post.image}`
+        : null,
+      postTitle: p.post.title,
+      price: Number(p.touristic.price),
     }));
 
     return successPaginatedResponse(results, total, page, items);
   }
-
 }
