700bf1a376578b0b6b2d571f8cd80326a8241e4f
Hasan <hzaeter04@gmail.com>
2025-08-16 17:19:53 +0300

    fix bugs

--- src/application/use-cases/advertisement/get-pending-ads.use-case.ts
+++ src/application/use-cases/advertisement/get-pending-ads.use-case.ts
@@ -1,30 +1,35 @@
-import { Inject, Injectable } from "@nestjs/common";
-import { ServiceType } from "src/domain/enums/service-type.enum";
-import { ADVERTISEMENT_REPOSITORY, AdvertisementRepositoryInterface } from "src/domain/repositories/advertisement.repository";
-import { ServicePriceRepository } from "src/infrastructure/repositories/service-price.repository";
+import { Inject, Injectable } from '@nestjs/common';
+import { ServiceType } from 'src/domain/enums/service-type.enum';
+import {
+  ADVERTISEMENT_REPOSITORY,
+  AdvertisementRepositoryInterface,
+} from 'src/domain/repositories/advertisement.repository';
+import { ServicePriceRepository } from 'src/infrastructure/repositories/service-price.repository';
 
 @Injectable()
 export class GetPendingAdvertisementUseCase {
-    constructor(
-        private readonly servicePriceRepo: ServicePriceRepository,
-        @Inject(ADVERTISEMENT_REPOSITORY)
-        private readonly advertisementRepo: AdvertisementRepositoryInterface,
-    ){}
+  constructor(
+    private readonly servicePriceRepo: ServicePriceRepository,
+    @Inject(ADVERTISEMENT_REPOSITORY)
+    private readonly advertisementRepo: AdvertisementRepositoryInterface,
+  ) {}
 
-    async execute(baseUrl: string){
-        const ads = await this.advertisementRepo.findPendingAds();
-        const price = await this.servicePriceRepo.findPriceByService(ServiceType.IMAGE_AD);
+  async execute(baseUrl: string) {
+    const ads = await this.advertisementRepo.findPendingAds();
+    const price = await this.servicePriceRepo.findPriceByService(
+      ServiceType.IMAGE_AD,
+    );
 
-        return ads.map((ad) => ({
-            id: ad.id,
-            amount: ad.day_period * price,
-            type: ServiceType.IMAGE_AD,
-            image:  `${baseUrl}/uploads/advertisements/images/${ad.image}`,
-            office_name: ad.office.name,
-            day_period: `${ad.day_period} أيام`,
-            created_at: ad.created_at.toISOString().split('T')[0]
-        }));
-    }
+    return ads.map((ad) => ({
+      id: ad.id,
+      amount: ad.day_period * price,
+      type: ServiceType.IMAGE_AD,
+      image: `${baseUrl}/uploads/advertisements/images/${ad.image}`,
+      office_name: ad.office.name,
+      day_period: `${ad.day_period} أيام`,
+      created_at: ad.created_at.toISOString().split('T')[0],
+    }));
+  }
 }
 
 // function getDayLabel(n: number): string {
--- src/presentation/http/swagger/advertisement/get-ad-request.swagger.ts
+++ src/presentation/http/swagger/advertisement/get-ad-request.swagger.ts
@@ -1,13 +1,13 @@
-
-import { applyDecorators } from "@nestjs/common";
-import { ApiBearerAuth, ApiOperation, ApiOkResponse } from "@nestjs/swagger";
+import { applyDecorators } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiOkResponse } from '@nestjs/swagger';
 
 export function GetAdRequestsSwaggerDoc() {
   return applyDecorators(
     ApiBearerAuth(),
     ApiOperation({
       summary: 'إرجاع جميع طلبات الإعلانات',
-      description: 'هذه الواجهة تُستخدم من قبل المشرف لإرجاع جميع طلبات الإعلانات الحالية التي لم يتم الدفع لها بعد.',
+      description:
+        'هذه الواجهة تُستخدم من قبل المشرف لإرجاع جميع طلبات الإعلانات الحالية التي لم يتم الدفع لها بعد.',
     }),
     ApiOkResponse({
       description: 'تم إرجاع جميع طلبات الإعلانات بنجاح',
@@ -19,21 +19,23 @@ export function GetAdRequestsSwaggerDoc() {
             {
               id: 1,
               amount: 1500,
-              type: 'IMAGE_AD',
-              image: 'http://localhost:3000/uploads/advertisements/images/ad1.png',
+              type: 'إعلان صوري',
+              image:
+                'http://localhost:3000/uploads/advertisements/images/ad1.png',
               office_name: 'مكتب دمشق',
               day_period: '5 أيام',
-              created_at: '2025-08-14T10:00:00.000Z'
+              created_at: '2025-08-14',
             },
             {
               id: 2,
               amount: 900,
-              type: 'IMAGE_AD',
-              image: 'http://localhost:3000/uploads/advertisements/images/ad2.png',
+              type: 'إعلان صوري',
+              image:
+                'http://localhost:3000/uploads/advertisements/images/ad2.png',
               office_name: 'مكتب حلب',
               day_period: '3 أيام',
-              created_at: '2025-08-13T14:30:00.000Z'
-            }
+              created_at: '2025-08-13',
+            },
           ],
           status_code: 200,
         },
