0e7f6916f21bcb3ca1647ccfad6ea6eaf6374389
Hasan <hzaeter04@gmail.com>
2025-08-18 06:10:08 +0300

    fix photo in signup api

--- src/application/use-cases/moblie_auth/create-user.usecase.ts
+++ src/application/use-cases/moblie_auth/create-user.usecase.ts
@@ -3,9 +3,15 @@ import { Injectable, ConflictException, Inject } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 
 import { CreateUserDto } from 'src/application/dtos/mobile_auth/create-user.dto';
-import { OtpService } from 'src/application/services/otp.service'; 
-import { USER_REPOSITORY, UserRepositoryInterface } from 'src/domain/repositories/user.repository';
-import { MOBILE_AUTH_REPOSITORY, MobileAuthRepositoryInterface } from 'src/domain/repositories/mobile_auth.repository';
+import { OtpService } from 'src/application/services/otp.service';
+import {
+  USER_REPOSITORY,
+  UserRepositoryInterface,
+} from 'src/domain/repositories/user.repository';
+import {
+  MOBILE_AUTH_REPOSITORY,
+  MobileAuthRepositoryInterface,
+} from 'src/domain/repositories/mobile_auth.repository';
 import { errorResponse } from 'src/shared/helpers/response.helper';
 
 @Injectable()
@@ -18,33 +24,41 @@ export class CreateUserUseCase {
     private readonly userRepo: UserRepositoryInterface,
 
     private readonly otpService: OtpService,
-    
   ) {}
   async execute(dto: CreateUserDto): Promise<void> {
     const existing = await this.userRepo.findByEmail(dto.email);
-    if (existing) throw new ConflictException(
-      errorResponse('البريد الإلكتروني مستخدم بالفعل',409)
-    );
+    if (existing)
+      throw new ConflictException(
+        errorResponse('البريد الإلكتروني مستخدم بالفعل', 409),
+      );
 
     const notActivated = await this.repoAuth.isTempUserExists(dto.email);
     if (notActivated) {
       throw new ConflictException(
-        errorResponse('تم إرسال رمز التحقق لهذا البريد مسبقاً. يرجى التحقق من بريدك أو إكمال التفعيل',409)
+        errorResponse(
+          'تم إرسال رمز التحقق لهذا البريد مسبقاً. يرجى التحقق من بريدك أو إكمال التفعيل',
+          409,
+        ),
       );
-    } 
+    }
     const hashed = await bcrypt.hash(dto.password, 10);
     await this.repoAuth.saveTempUser({
       first_name: dto.first_name,
-      last_name:  dto.last_name,
-      phone:      dto.phone,
-      photo:      dto.photo,     
-      email:      dto.email,
-      password:   hashed,
+      last_name: dto.last_name,
+      phone: dto.phone,
+      photo: dto.photo,
+      email: dto.email,
+      password: hashed,
     });
-   
+
     const code = this.otpService.generateOtp();
     const expiresAt = new Date(Date.now() + 5 * 60_000);
-    await this.repoAuth.saveOtp({ email: dto.email, code, type: 'signup', expires_at: expiresAt });
+    await this.repoAuth.saveOtp({
+      email: dto.email,
+      code,
+      type: 'signup',
+      expires_at: expiresAt,
+    });
     this.otpService.sendOtp(dto.email, code);
   }
 }
--- src/application/use-cases/moblie_auth/verify-otp.use-case.ts
+++ src/application/use-cases/moblie_auth/verify-otp.use-case.ts
@@ -1,10 +1,9 @@
 // src/application/use-cases/confirm-signup.use-case.ts
-import { Injectable, BadRequestException,Inject } from '@nestjs/common';
- 
-import { OtpType }        from 'src/domain/entities/otp.entity';
-import { User }        from 'src/domain/entities/user.entity';  
-import { VerifyOtpDto } from 'src/application/dtos/mobile_auth/verify-otp.dto'; 
-  
+import { Injectable, BadRequestException, Inject } from '@nestjs/common';
+
+import { OtpType } from 'src/domain/entities/otp.entity';
+import { User } from 'src/domain/entities/user.entity';
+import { VerifyOtpDto } from 'src/application/dtos/mobile_auth/verify-otp.dto';
 
 import {
   MOBILE_AUTH_REPOSITORY,
@@ -27,31 +26,31 @@ export class VerifyOtpUseCase {
   ) {}
   async execute(dto: VerifyOtpDto): Promise<void> {
     const now = new Date();
-   
+
     const temp = await this.repo.findTempUserByEmail(dto.email);
     if (!temp) {
       throw new BadRequestException(
-        errorResponse('لم يُسجّل هذا البريد أو لم يُرسل إليه رمز تحقق',400)
-      );    }
+        errorResponse('لم يُسجّل هذا البريد أو لم يُرسل إليه رمز تحقق', 400),
+      );
+    }
 
     const otp = await this.repo.findLatestValidOtp(dto.email, 'signup', now);
     if (!otp || otp.code !== dto.otp) {
       throw new BadRequestException(
-        errorResponse('رمز التحقق غير صالح أو انتهت صلاحيته',400)
+        errorResponse('رمز التحقق غير صالح أو انتهت صلاحيته', 400),
       );
-    }  
-   
+    }
+
     await this.userRepo.save({
       first_name: temp.first_name,
-      last_name:  temp.last_name,
-      phone:      temp.phone,
-      photo:      temp.photo,
-      email:      temp.email,
-      password:   temp.password,
+      last_name: temp.last_name,
+      phone: temp.phone,
+      photo: temp.photo,
+      email: temp.email,
+      password: temp.password,
     } as User);
-   
+
     await this.repo.deleteTempUserByEmail(dto.email);
     await this.repo.deleteOtp(dto.email, 'signup');
   }
-  
 }
