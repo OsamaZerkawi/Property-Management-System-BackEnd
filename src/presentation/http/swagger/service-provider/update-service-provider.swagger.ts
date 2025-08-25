688f9ea719feb2b10acc5d5b77d2911cec8ec764
onlyone <abdallaalkasm9@gmail.com>
2025-08-26 00:08:24 +0300

    swagger update.

--- src/presentation/http/swagger/service-provider/update-service-provider.swagger.ts
+++ src/presentation/http/swagger/service-provider/update-service-provider.swagger.ts
@@ -29,10 +29,10 @@ export function UpdateServiceProviderSwagger() {
           opening_time: { type: 'string', example: '09:00', description: 'وقت الافتتاح بصيغة HH:mm' },
           closing_time: { type: 'string', example: '22:00', description: 'وقت الإغلاق بصيغة HH:mm' },
           details: { type: 'string', example: 'وصف مختصر للخدمة', description: 'تفاصيل مزود الخدمة' },
-          status: { type: 'boolean', example: '1', description: 'هل الخدمة فعالة ام لا' },
+          status: { type: 'boolean', example: 'true', description: 'هل الخدمة فعالة ام لا' },
           career: {
             type: 'string',
-            example: 'RESTAURANT',
+            example: 'كهربائي',
             description:
               'نوع مزود الخدمة (استخدم إحدى قيم enum ServiceProviderType المتاحة في مشروعك).',
           },
